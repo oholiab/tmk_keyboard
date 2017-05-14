d55bcb8ee3abb36c6d0c9feedc257ab99a2ad2e0
oholiab <oholiab@grimmwa.re>
2017-05-14 16:39:29 +0100

    Some untested changes to grimmboard keymap

--- keyboard/grimmboard/keymap.c
+++ keyboard/grimmboard/keymap.c
@@ -27,12 +27,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "keymap.h"
 
 
+/* Need to find space for:
+ * ENTER: FN SHIFT
+ * BS: FN SPACE
+ * DEL: ALT SPACE
+ * SUPER: FN CTRL
+ * ARROWS: FN WASD
+ * PGUP/DOWN: FN JK
+ * HOME/END: FN HL
+ * TAB: FN Q
+ * ESC: SINGLE CTRL/FN ALT
+ * F1-12: FN top row
+ */
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {
-      {KC_A, KC_B, KC_C, KC_D, KC_E, KC_F, KC_G, KC_H},
-      {KC_I, KC_J, KC_K, KC_L, KC_M, KC_N, KC_O, KC_P},
-      {KC_Q, KC_R, KC_S, KC_T, KC_U, KC_V, KC_W, KC_X},
-      {KC_LCTRL, KC_Y, KC_Z, KC_TAB, KC_ESC, KC_DELETE, KC_SPACE, KC_LSHIFT},
+      {FN,    KC_1,	KC_2,	KC_3,	KC_4,	KC_5,	KC_6,	KC_7,	KC_8,			KC_9,			KC_0,			KC_MINS,	KC_EQL},
+      {SHIFT, KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,	KC_Y,	KC_U,	KC_I,			KC_O,			KC_P,			KC_LBRC,	KC_RBRC},
+      {CTRL,  KC_A,	KC_S,	KC_D,	KC_F,	KC_G,	KC_H,	KC_J,	KC_K,			KC_L,			KC_SCLN,	KC_QUOT,  KC_BSLS},
+      {ALT,   KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,	KC_N,	KC_M,	KC_COMMA,	KC_DOT, 	KC_SLSH,  KC_GRV,   KC_SPACE},
     },
 };
 
